30878048406581ceaf00e7116fa8420bbcc920ba
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2016-08-11 22:57:11 +0700

    - Create class `AddNewName`.

--- FlaskDemo.py
+++ FlaskDemo.py
@@ -8,6 +8,15 @@ class HelloWorld(Resource):
     def get(self):
         return {'Message': 'Hello World'}
 
+class AddNewName(Resource)
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('name', type=str, help='Name to add new name')
+		args = parser.parse_args()
+
+		return {'Message': 'Add '+args['name']+' Success !!'}
+
+
 api.add_resource(HelloWorld, '/HelloWorld')
 
 if __name__ == '__main__':
